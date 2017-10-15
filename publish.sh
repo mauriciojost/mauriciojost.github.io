b7544da67a08ade2f404ee5252f5ae9eb2e1c742
Mauricio Jost <mauricio.jost@gmail.com>
2017-10-15 19:55:54 +0200

    Minor refactoring on publish.sh

--- publish.sh
+++ publish.sh
@@ -6,10 +6,14 @@ set -e
 curr_dir="$(readlink -e `dirname $0`)"
 root_dir="$curr_dir"
 site_dir="$root_dir/_site"
+site_dir="$root_dir/_site"
 site_tgz_tmp="/tmp/publish-$RANDOM-$RANDOM.tar.gz"
 date_now="$(date)"
 
 git checkout development
+rm -fr "$site_dir"
+mkdir -p "$site_dir"
+cp 
 jekyll build
 
 ls -lah "$site_dir"
@@ -23,6 +27,7 @@ cd "$root_dir"
 rm -fr *
 tar -xf "$site_tgz_tmp"
 git commit -a -s -m "Publish site as of $date_now"
+git push -f origin master
 
 git checkout -f development
 
