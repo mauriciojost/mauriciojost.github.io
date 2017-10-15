2da53be1d514ba595340f607c495b6faad322dd0
Mauricio Jost <mauricio.jost@gmail.com>
2017-10-15 19:23:29 +0200

    Add publish.sh script

---
+++ publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+curr_dir="$(readlink -e `dirname $0`)"
+root_dir="$curr_dir"
+site_dir="$root_dir/_site"
+site_tgz_tmp="/tmp/publish-$RANDOM-$RANDOM.tar.gz"
+date_now="$(date)"
+
+git checkout development
+jekyll build
+
+ls -lah "$site_dir"
+cd "$site_dir"
+tar -czf "$site_tgz_tmp" *
+cd "$root_dir"
+rm -fr "$site_dir"
+
+git checkout master
+cd "$root_dir"
+rm -fr *
+tar -xf "$site_tgz_tmp"
+git commit -a -s -m "Publish site as of $date_now"
+
+git checkout development
+
